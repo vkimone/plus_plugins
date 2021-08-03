72ff0e61585fdb01c0a0bf5eeadf17d24eb2025a
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-08-03 15:39:12 +0900

    Add private keyword in AlarmFlagManager.java

--- packages/android_alarm_manager_plus/android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmFlagManager.java
+++ packages/android_alarm_manager_plus/android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmFlagManager.java
@@ -9,9 +9,9 @@ public class AlarmFlagManager {
   private static final String FLUTTER_SHARED_PREFERENCE_KEY = "FlutterSharedPreferences";
   private static final String ALARM_FLAG_KEY = "flutter.alarmFlagKey";
 
-  static void set(Context context, Intent intent) {
+  static public void set(Context context, Intent intent) {
     int callbackId = intent.getIntExtra("id", -1);
-    long alarmId = _getActualIdFrom(callbackId);
+    long alarmId = getActualIdFrom(callbackId);
 
     SharedPreferences prefs = context.getSharedPreferences(FLUTTER_SHARED_PREFERENCE_KEY, 0);
     prefs.edit().putLong(ALARM_FLAG_KEY, alarmId).apply();
@@ -22,7 +22,7 @@ public class AlarmFlagManager {
    *
    * 이때 `Timer`의 경우는 ID가 0이므로 반환값도 0이 되어 문제가 없다.
    */
-  static long _getActualIdFrom(long callbackId) {
+  static private long getActualIdFrom(long callbackId) {
     return (long) Math.floor(callbackId / 7.0);
   }
 
