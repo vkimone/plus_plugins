28f6a6f24611609a2eb655b0c3280bef931f7f7a
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-05-25 19:39:58 +0900

    Remove getActualIdFrom function

--- packages/android_alarm_manager_plus/android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmFlagManager.java
+++ packages/android_alarm_manager_plus/android/src/main/java/dev/fluttercommunity/plus/androidalarmmanager/AlarmFlagManager.java
@@ -11,19 +11,9 @@ public class AlarmFlagManager {
 
   static public void set(Context context, Intent intent) {
     int callbackId = intent.getIntExtra("id", -1);
-    long alarmId = getActualIdFrom(callbackId);
 
     SharedPreferences prefs = context.getSharedPreferences(FLUTTER_SHARED_PREFERENCE_KEY, 0);
-    prefs.edit().putLong(ALARM_FLAG_KEY, alarmId).apply();
-  }
-
-  /**
-   * 알람 객체의 ID로 변환한 ID를 반환한다.
-   *
-   * 이때 `Timer`의 경우는 ID가 0이므로 반환값도 0이 되어 문제가 없다.
-   */
-  static private long getActualIdFrom(long callbackId) {
-    return (long) Math.floor(callbackId / 7.0);
+    prefs.edit().putLong(ALARM_FLAG_KEY, callbackId).apply();
   }
 
 }
